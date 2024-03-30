92dd102e3838c9600fc67a0ec6165b976f35e1a3
maxi33267017 <85323502+maxi33267017@users.noreply.github.com>
2024-03-29 22:38:37 -0300

    nuevo acceso

--- vite-project/src/components/categories/CategoryComponent.jsx
+++ vite-project/src/components/categories/CategoryComponent.jsx
@@ -1,14 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { getProducts } from '../../asyncMock';
 import { useEffect, useState } from 'react';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, handleClick }) => {
   return (
     <div className="card">
       <img src={product.image} className="card-img-top img-fluid imgProduct" alt={product.title} />
       <div className="card-body">
         <h5 className="card-title">{product.title}</h5>
         <p className="card-text">Precio: ${product.price}</p>
+        <button className="btn custom-button" onClick={() => handleClick(product.id)}>Ver detalles</button>
       </div>
     </div>
   );
@@ -17,13 +18,26 @@ const ProductCard = ({ product }) => {
 export default function CategoryComponent() {
   const { catName } = useParams();
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  
+  const handleClick = (id) => {
+    navigate(`/product/${id}`);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getProducts;
-      if (catName === 'Maquinarias' || catName === 'Repuestos') {
-        const filteredProducts = data.filter(product => product.category === catName);
-        setProducts(filteredProducts);
+      try {
+        const data = await getProducts; // Invoke getProducts as a Promise
+        if (catName === 'Maquinarias' || catName === 'Repuestos') {
+          const filteredProducts = data.filter(product => product.category === catName);
+          setProducts(filteredProducts);
+        } else {
+          // Handle case when category name doesn't match
+          setProducts([]);
+        }
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        // Handle error as necessary
       }
     };
 
@@ -36,7 +50,7 @@ export default function CategoryComponent() {
       <div className="row">
         {products.map(product => (
           <div key={product.id} className="col-md-4">
-            <ProductCard product={product} />
+            <ProductCard product={product} handleClick={handleClick} />
           </div>
         ))}
       </div>
